fea67ae36a6f0b15918533ea38ed275be1948777
axelescalante0 <141271318+axelescalante0@users.noreply.github.com>
2023-08-21 18:49:42 -0300

    creo carpeta 'ListaDoblementeEnlazada'

---
+++ ayed-tps-codigos-iniciales-para-estudiantes/ListaDoblementeEnlazada/lista_doblemente_enlazada.py
@@ -0,0 +1,41 @@
+from nodo import Nodo
+
+class ListaDoblementeEnlazada:
+    def __init__(self):
+        self.primero = None
+        self.ultimo = None
+        self.size = 0
+    
+    def vacia(self):
+        return self.primero == None
+    
+    def agregar_final(self, dato):
+        if self.vacia():        #si la lista esta vacia el primer nodo agredado sera el primer y ultimo nodo
+            self.primero = self.ultimo = Nodo(dato)
+        else:  # caso contrario nuestra lista ya contiene un nodos
+            aux = self.ultimo
+            self.ultimo = aux.siguiente = Nodo(dato)
+            self.ultimo.anterior = aux
+        self.size += 1
+    
+    def agregar_inicio(self, dato):
+        if self.vacia():
+            self.primero = self.ultimo = Nodo(dato)
+        else:
+            aux = Nodo(dato)
+            aux.siguiente = self.primero
+            self.primero.anterior = aux
+            self.primero = aux
+        self.size +=1
+
+    def recorrer_inicio(self):
+        aux = self.primero
+        while aux:
+            print(aux.dato)
+            aux = aux.siguiente
+    
+    def recorrer_fin(self):
+        aux = self.ultimo
+        while aux:
+            print(aux.dato)
+            aux = aux.anterior
