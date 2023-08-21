ac675fa766a94ac6f9ba77d6cb7c244261c34a99
axelescalante0 <141271318+axelescalante0@users.noreply.github.com>
2023-08-21 19:01:40 -0300

    Guardo los datos

---
+++ ayed-tps-codigos-iniciales-para-estudiantes/ListaDoblementeEnlazada/main.py
@@ -0,0 +1,13 @@
+from lista_doblemente_enlazada import ListaDoblementeEnlazada
+
+lista = ListaDoblementeEnlazada()
+
+lista.agregar_final(2)
+lista.agregar_final(4)
+lista.agregar_final(6)
+lista.agregar_final(8)
+lista.agregar_final(10)
+
+lista.recorrer_inicio()
+
+
