9a0ea40e68a49d587951952226a1965bbd1606a9
axelescalante0 <141271318+axelescalante0@users.noreply.github.com>
2023-08-23 10:19:50 -0300

    Update main.py
    Prueba de nuevos metodos.

--- ayed-tps-codigos-iniciales-para-estudiantes/ListaDoblementeEnlazada/main.py
+++ ayed-tps-codigos-iniciales-para-estudiantes/ListaDoblementeEnlazada/main.py
@@ -2,12 +2,13 @@ from lista_doblemente_enlazada import ListaDoblementeEnlazada
 
 lista = ListaDoblementeEnlazada()
 
-lista.agregar_final(2)
-lista.agregar_final(4)
-lista.agregar_final(6)
-lista.agregar_final(8)
-lista.agregar_final(10)
+lista.agregar_al_final(2)
+lista.agregar_al_inicio(1)
 
+print('tamanio de las lista:',lista.tamanio())
+
+
+print('--------recorer de inicio a fin--------')
 lista.recorrer_inicio()
-
-
+print('--------recorer de fin a inicio--------')
+lista.recorrer_fin()
